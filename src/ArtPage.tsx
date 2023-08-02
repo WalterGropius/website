ef45e3abc63ea6c8463ff820541387b5aeb69427
Eliáš Bauer <zenbauhaus@gmail.com>
2023-08-03 00:24:19 +0200

    1.5

--- src/ArtPage.tsx
+++ src/ArtPage.tsx
@@ -4,7 +4,7 @@ const ArtPage: React.FC = () => {
   return (
     
       <embed
-  src="http://zenbau.haus/portfolio.pdf#toolbar=0&navpanes=0&view=FitH"
+  src="https://zenbau.haus/portfolio.pdf#toolbar=0&navpanes=0&view=FitH"
   width="100%" height="100%" />
 
   );
--- src/CVPage.tsx
+++ src/CVPage.tsx
@@ -6,7 +6,7 @@ const CVPage: React.FC = () => {
   return (
     
       <embed
-  src="http://zenbau.haus/cv.pdf#toolbar=0&navpanes=0&view=FitH"
+  src="https://zenbau.haus/cv.pdf#toolbar=0&navpanes=0&view=FitH"
   width="100%" height="100%" />
 
   );
