ea4d19425c6038eb2dfa361965983b61454ba7d6
Eliáš Bauer <zenbauhaus@gmail.com>
2023-08-07 23:01:38 +0200

    jjhvj
    lknnk

--- src/ProjectPage.tsx
+++ src/ProjectPage.tsx
@@ -25,14 +25,22 @@ function ProjectPage() {
   if (!item) return <div>Loading...</div>;
 
   return (
-    <div>
-     
-      <a href={item.link} target="_blank" rel="noopener noreferrer"> <img src={item.image} />
+    <div
+      style={{
+        width: '100%', // Set the width to 100% to make it full-width
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center', // Center-align the content horizontally
+      }}
+    >
+      <a href={item.link} target="_blank" rel="noopener noreferrer">
+        <img src={item.image} style={{ maxWidth: '100%', height: 'auto' }} />
+      </a>
       <h1>{item.title}</h1>
       <p>{item.date}</p>
-      <p>{item.description}</p></a>
+      <p>{item.description}</p>
       <p>{item.tags}</p>
-      <Link to={"/work"}>back</Link>
+      <Link to={"/work"}>Back</Link>
       {/* More item details here */}
     </div>
   );
