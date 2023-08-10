9b500d3006dcaedfc78f4acbd45d510e5c8d1761
Eliáš Bauer <zenbauhaus@gmail.com>
2023-08-10 23:17:18 +0200

    dfs
    fsdfsf

--- src/ArtPage.tsx
+++ src/ArtPage.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import ImageCarousel from './ImageCarousel';
-const images = [
-  "/image01.png",
-  "/image02.png",
-  // ... more images
-];
+
 const ArtPage: React.FC = () => {
   return (
     <div>
         <h1>My Carousel</h1>
-        <ImageCarousel images={images} />
+        
     </div>
 );
 }
